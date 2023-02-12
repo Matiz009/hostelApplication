db69dbce3d12b63adb01a9ed409cd804c11a77d3
MATI UL REHMAN <iammatiz00@gmail.com>
2023-02-12 19:35:18 +0500

    front end completed 45:40

--- frontend/src/components/ViewApplicant.jsx
+++ frontend/src/components/ViewApplicant.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 const ViewApplicant = () => {
   return (
-    <div>
+    <div className="col-md-6 position-absolute start-50 translate-middle-x mt-5">
       {" "}
-      <h1> View </h1>{" "}
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Student Name</th>
+            <th scope="col">Registration Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
